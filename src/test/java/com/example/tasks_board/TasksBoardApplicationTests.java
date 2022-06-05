e0f41a4d3a3892cd5763e3bdb259dc76fcea81e4
Cronion <ohmytarik@mail.ru>
2022-06-05 18:56:07 +0300

    initial commit

---
+++ src/test/java/com/example/tasks_board/TasksBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasks_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
